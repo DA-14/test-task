e1d04421f914c1a2e282246bd151df9694ce2989
Anton Vidinev <anton.vidinev@da-14.com>
2019-09-30 12:10:05 +0300

    feature(input-indicator) use indicator component in temp cmp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-
-import { selectNewYorkTemperature } from './store/reducers/temperature.reducer';
-import { AppState } from './store/interfaces/app-state.interface';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  newYorkTemperature$: Observable<number>;
-
-  constructor(private store: Store<AppState>) {
-  }
-
-  ngOnInit() {
-    this.newYorkTemperature$ = this.store.select(selectNewYorkTemperature);
-  }
+export class AppComponent {
 }
